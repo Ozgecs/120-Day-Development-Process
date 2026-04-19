1611579a948401c4bb67004ceaaaa93b6b135447
Ozgecs <ozgeshn13@gmail.com>
2026-04-19 15:44:55 +0300

    Soru -2

--- src/Gun1/Soru_2_K.java
+++ src/Gun1/Soru_2_K.java
@@ -1,4 +1,25 @@
 package Gun1;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Soru_2_K {
+    public static void main(String[] args){
+
+        // 1 den 20 ye kadar olan sayılar yazdırılır.
+        // Sadece çift olanlar gösterilir.
+
+        List<Integer> even_num = new ArrayList<>();
+
+        for (int i = 1; i<= 20; i++){
+            if (i % 2 == 0){
+                even_num.add(i);
+            }
+        }
+
+        for (Integer e_num: even_num){
+            System.out.println(e_num);
+        }
+    }
 }
